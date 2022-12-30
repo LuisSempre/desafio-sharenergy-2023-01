1ce16c93c515e3e1be36079d59618bb5b4e0a2ea
LuisSempre <luis.silva@viannasempre.com.br>
2022-12-29 22:43:00 -0300

    Install TailwindCSS

---
+++ desafio-sharenergy-2023/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className='bg-black w-full h-screen text-white justify-center items-center flex'>
+      Desafio 2023
+    </div>
+  );
+}
+
+export default App;
