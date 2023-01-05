07060e3789c4ade79a865770f9f3e46f92eaaaa2
LuisSempre <luis.silva@viannasempre.com.br>
2023-01-04 21:21:16 -0300

    Remove Login

--- desafio-sharenergy-2023/src/App.tsx
+++ desafio-sharenergy-2023/src/App.tsx
@@ -3,33 +3,15 @@ import Four from "./pages/Four";
 import Home from "./pages/Home";
 import Two from "./pages/Two";
 import Three from "./pages/Three";
-import { RequireAuth } from "./contexts/Auth/RequireAuth";
-import { AuthContext } from "./contexts/Auth/AuthContext";
 import { useContext } from "react";
 
 export default function App() {
-  const auth = useContext(AuthContext);
-
-  const handleLogout = async () => {
-    await auth.signout();
-    window.location.href = window.location.href;
-  };
   return (
     <>
-      <header>
-        <nav>{auth.user && <button onClick={handleLogout}>Sair</button>}</nav>
-      </header>
       <Router>
         <div className='pt-20'>
           <Routes>
-            <Route
-              path='/'
-              element={
-                <RequireAuth>
-                  <Home />
-                </RequireAuth>
-              }
-            />
+            <Route path='/' element={<Home />} />
             <Route path='/Two' element={<Two />} />
             <Route path='/Three' element={<Three />} />
             <Route path='/four' element={<Four />} />
--- desafio-sharenergy-2023/src/main.tsx
+++ desafio-sharenergy-2023/src/main.tsx
@@ -2,15 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { AuthProvider } from "./contexts/Auth/AuthProvider";
-import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <AuthProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </AuthProvider>
+    <App />
   </React.StrictMode>,
 );
